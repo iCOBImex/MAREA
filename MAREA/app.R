b4daaa3d5ac2a51dd9eb6c5cea660a903c02f1a5
jcvdav <juancarlos.villader@gmail.com>
2017-05-09 18:51:28 -0700

    Now the sidebar is collapsed when MAREA launches

--- MAREA/app.R
+++ MAREA/app.R
@@ -297,6 +297,8 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
   
+  addClass(selector = "body", class = "sidebar-collapse")
+  
   #### Manage the buttons to go back and forth ####
   # The way to read this is that b1.2 changes from tab 1 to tab 2. As
   # b6.5 changes from tab 6 to tab 5.
